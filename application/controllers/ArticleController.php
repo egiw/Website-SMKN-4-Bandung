6ceb6b9991c7aeb4490916824d5cf158509d05de
Egi Soleh Hasdi <egisolehhasdi@gmail.com>
2013-02-12 20:33:04 +0700

    implementasi komentar, don't forget to import comment.sql

--- application/controllers/ArticleController.php
+++ application/controllers/ArticleController.php
@@ -15,11 +15,36 @@ class ArticleController extends Zend_Controller_Action
   public function viewAction()
   {
     $id = $this->getParam('id');
+    $pageNumber = $this->getParam('page');
     if (null !== $id) {
+      $form = new Application_Form_Comment();
       $model = new Application_Model_DbTable_Article();
+      $comment = new Application_Model_DbTable_Comment();
       $article = $model->find($id)->current();
+
       if (null !== $article) {
+        if ($this->getRequest()->isPost()) {
+          $data = $this->getRequest()->getPost();
+          if ($form->isValid($data)) {
+            $comment->insert(array(
+                'user' => Zend_Auth::getInstance()->getIdentity()->username,
+                'created_on' => Date('Y-m-d H:i:s'),
+                'content' => $form->content->getValue(),
+                'article_id' => $article->id
+            ));
+            $form->content->setValue('');
+          }
+        }
+
+
+        $comments = Zend_Paginator::factory($comment->findArticleComments($article->id));
+        $comments->setItemCountPerPage(5);
+        $comments->setCurrentPageNumber($pageNumber);
+
+        $this->view->comments = $comments;
         $this->view->article = $article->toArray();
+        $this->view->auth = Zend_Auth::getInstance();
+        $this->view->form = $form;
       }
     }
   }
---
+++ application/forms/Comment.php
@@ -0,0 +1,52 @@
+<?php
+
+class Application_Form_Comment extends Zend_Form
+{
+  const CONTENT_IS_EMPTY = 'Komentar tidak boleh kosong.';
+
+  /**
+   *
+   * @var Zend_Form_Element_Textarea
+   */
+  public $content;
+  /**
+   *
+   * @var Zend_Form_Element_Submit
+   */
+  public $submit;
+
+  public function init()
+  {
+    $this->addPrefixPath('SITi_Form_Decorator', 'SITi/Form/Decorator', 'Decorator');
+    $this->content = new Zend_Form_Element_Textarea('content');
+    $this->submit = new Zend_Form_Element_Submit('submit');
+
+    $this->content
+            ->setRequired(true)
+            ->setValidators(array(
+                array('NotEmpty', false, array(
+                        'messages' => array(
+                            Zend_Validate_NotEmpty::IS_EMPTY => self::CONTENT_IS_EMPTY
+                        )
+                ))
+            ))
+            ->setFilters(array(
+                array('StringTrim'),
+                array('StripNewLines'),
+                array('HtmlEntities'),
+            ));
+
+    $this->submit
+            ->setLabel('Kirim')
+            ->setAttrib('class', 'btn');
+
+    $this->addElements(array(
+        $this->content,
+        $this->submit
+    ));
+    $this->submit->setDecorators(array('ViewHelper'));
+    $this->content->setDecorators(array('ViewHelper', array('Errors', array(
+                'class' => 'alert alert-error unstyled', 'style' => 'font-size: 12px'))));
+  }
+
+}
