91d96a00f2f5484416c0edb59af3aa27c830be1f
Egi Soleh Hasdi <egisolehhasdi@gmail.com>
2013-02-16 12:08:00 +0700

    modifikasi artikel, prakerin

--- application/controllers/ArticleController.php
+++ application/controllers/ArticleController.php
@@ -2,51 +2,80 @@
 
 class ArticleController extends Zend_Controller_Action
 {
-  public function init()
-  {
-    /* Initialize action controller here */
-  }
-
-  public function indexAction()
-  {
-    
-  }
-
-  public function viewAction()
-  {
-    $id = $this->getParam('id');
-    $pageNumber = $this->getParam('page');
-    if (null !== $id) {
-      $form = new Application_Form_Comment();
-      $model = new Application_Model_DbTable_Article();
-      $comment = new Application_Model_DbTable_Comment();
-      $article = $model->find($id)->current();
-
-      if (null !== $article) {
-        if ($this->getRequest()->isPost()) {
-          $data = $this->getRequest()->getPost();
-          if ($form->isValid($data)) {
-            $comment->insert(array(
-                'user' => Zend_Auth::getInstance()->getIdentity()->username,
-                'created_on' => Date('Y-m-d H:i:s'),
-                'content' => $form->content->getValue(),
-                'article_id' => $article->id
-            ));
-            $form->content->setValue('');
-          }
-        }
-
-
-        $comments = Zend_Paginator::factory($comment->findArticleComments($article->id));
-        $comments->setItemCountPerPage(15);
-        $comments->setCurrentPageNumber($pageNumber);
-
-        $this->view->comments = $comments;
-        $this->view->article = $article->toArray();
-        $this->view->auth = Zend_Auth::getInstance();
-        $this->view->form = $form;
-      }
+    public function init()
+    {
+        /* Initialize action controller here */
     }
-  }
+
+    public function indexAction()
+    {
+        $model = new Application_Model_DbTable_Article();
+        $data = $model->findAll();
+        $articles = Zend_Paginator::factory($data);
+        $this->view->articles = $articles;
+    }
+
+    public function viewAction()
+    {
+        $id = $this->getParam('id');
+        $pageNumber = $this->getParam('page');
+        if (null !== $id) {
+            $form = new Application_Form_Comment();
+            $model = new Application_Model_DbTable_Article();
+            $comment = new Application_Model_DbTable_Comment();
+            $article = $model->find($id)->current();
+
+            if (null !== $article) {
+                if ($this->getRequest()->isPost()) {
+                    $data = $this->getRequest()->getPost();
+                    if ($form->isValid($data)) {
+                        $comment->insert(array(
+                            'user'       => Zend_Auth::getInstance()->getIdentity()->username,
+                            'created_on' => Date('Y-m-d H:i:s'),
+                            'content'    => $form->content->getValue(),
+                            'article_id' => $article->id
+                        ));
+                        $form->content->setValue('');
+                    }
+                }
+
+                if (!$this->getRequest()->getCookie('view_article_' . $id)) {
+                    $article->views += 1;
+                    $article->save();
+                    setcookie('view_article_' . $id, true, time() + 60 * 60 * 24, '/');
+                }
+
+                $comments = Zend_Paginator::factory($comment->findArticleComments($article->id));
+                $comments->setItemCountPerPage(15);
+                $comments->setCurrentPageNumber($pageNumber);
+
+                $this->view->comments = $comments;
+                $this->view->article = $article->toArray();
+                $this->view->auth = Zend_Auth::getInstance();
+                $this->view->form = $form;
+            }
+        }
+    }
+
+    public function likeAction()
+    {
+        $this->_helper->layout->disableLayout();
+        $this->_helper->viewRenderer->setNoRender();
+        $id = $this->getParam('id');
+        if (null !== $id && Zend_Auth::getInstance()->hasIdentity()) {
+            $model = new Application_Model_DbTable_Article();
+            $article = $model->find($id)->current();
+            if (null !== $article) {
+                $username = Zend_Auth::getInstance()->getIdentity()->username;
+                $likes = array_unique(explode(',', $article->likes));
+                if (!in_array($username, $likes)) {
+                    $likes[] = $username;
+                }
+                $article->likes = ltrim(implode(',', $likes), ',');
+                $article->save();
+            }
+        }
+        $this->redirect('/article/view/id/' . $id . '#comment');
+    }
 
 }
--- application/models/DbTable/Prakerin.php
+++ application/models/DbTable/Prakerin.php
@@ -2,28 +2,28 @@
 
 class Application_Model_DbTable_Prakerin extends Zend_Db_Table_Abstract
 {
-  protected $_name = 'prakerin';
+    protected $_name = 'prakerin';
 
-  public function findAll($filter = null)
-  {
-    $select = $this->select()->from($this->_name);
+    public function findAll($filter = null)
+    {
+        $select = $this->select()->from($this->_name);
 
-    if (null !== $filter) {
-      if (null != $filter['name']) {
-        $select->where("{$this->_name}.name LIKE ?", "%{$filter['name']}%");
-      }
-      if (null != $filter['address']) {
-        $select->where("{$this->_name}.address LIKE ?", "%{$filter['address']}%");
-      }
-      if (isset($filter['category']) && null != $filter['category']) {
-        foreach ($filter['category'] as $category) {
-          $select->orWhere("{$this->_name}.category LIKE ?", "%{$category}%");
+        if (null !== $filter) {
+            if (isset($filter['name']) && null != $filter['name']) {
+                $select->where("{$this->_name}.name LIKE ?", "%{$filter['name']}%");
+            }
+            if (isset($filter['address']) && null != $filter['address']) {
+                $select->where("{$this->_name}.address LIKE ?", "%{$filter['address']}%");
+            }
+            if (isset($filter['category']) && null != $filter['category']) {
+                foreach ($filter['category'] as $category) {
+                    $select->orWhere("{$this->_name}.category LIKE ?", "%{$category}%");
+                }
+            }
         }
-      }
-    }
 
-    $result = $this->fetchAll($select);
-    return $result;
-  }
+        $result = $this->fetchAll($select);
+        return $result;
+    }
 
 }
