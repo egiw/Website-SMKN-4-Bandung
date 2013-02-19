f5504e2cec752e66dd8a95ea9eeb30e53e4cacbe
Egi Soleh Hasdi <egisolehhasdi@gmail.com>
2013-02-19 17:53:43 +0700

    changes di article

--- application/modules/admin/models/DbTable/Article.php
+++ application/modules/admin/models/DbTable/Article.php
@@ -2,53 +2,54 @@
 
 class Admin_Model_DbTable_Article extends Zend_Db_Table_Abstract
 {
-  protected $_name = 'article';
+    protected $_name = 'article';
 
-  public function findAll($user = null, $filter = NULL)
-  {
-    $select = $this->select()->setIntegrityCheck(false)
-            ->from($this->_name);
+    public function findAll($user = null, $filter = NULL)
+    {
+        $select = $this->select()->setIntegrityCheck(false)
+        ->from($this->_name)
+        ->columns(array('comments' => "(SELECT COUNT(*) FROM comment WHERE article_id = {$this->_name}.id)"));
 
-    if (null !== $user) {
-      $select->where("{$this->_name}.created_by = ?", $user);
+        if (null !== $user) {
+            $select->where("{$this->_name}.created_by = ?", $user);
+        }
+
+        if (Admin_Model_Status::ARCHIVED != $filter['status']) {
+            $select->where("{$this->_name}.status != ?", Admin_Model_Status::ARCHIVED);
+        }
+
+        if (null !== $filter) {
+            if (null != $filter ['title']) {
+                $select->where("{$this->_name}.title like ?", "%{$filter['title']}%");
+            }
+            if (null != $filter['tag']) {
+                $select->where("{$this->_name}.tags like ?", "%{$filter['tag']}%");
+            }
+            if (null != $filter['status']) {
+                $select->where("{$this->_name}.status = ?", $filter['status']);
+            }
+        }
+
+        $select->order("{$this->_name}.created_on DESC");
+
+        $result = $this->fetchAll($select);
+        return $result;
     }
 
-    if (Admin_Model_Status::ARCHIVED != $filter['status']) {
-      $select->where("{$this->_name}.status != ?", Admin_Model_Status::ARCHIVED);
+    public function countStatus($username = null)
+    {
+        $select = $this->select()->from($this->_name, array(
+            'archived' => "SUM(status = 'archived')",
+            'draft'    => "SUM(status = 'draft')",
+            'pending'  => "SUM(status = 'pending')",
+            'publish'  => "SUM(status = 'publish')"));
+
+        if (null !== $username) {
+            $select->where("{$this->_name}.created_by = ?", $username);
+        }
+
+        $result = $this->getAdapter()->fetchRow($select);
+        return $result;
     }
-
-    if (null !== $filter) {
-      if (null != $filter ['title']) {
-        $select->where("{$this->_name}.title like ?", "%{$filter['title']}%");
-      }
-      if (null != $filter['tag']) {
-        $select->where("{$this->_name}.tags like ?", "%{$filter['tag']}%");
-      }
-      if (null != $filter['status']) {
-        $select->where("{$this->_name}.status = ?", $filter['status']);
-      }
-    }
-    
-    $select->order("{$this->_name}.created_on DESC");
-
-    $result = $this->fetchAll($select);
-    return $result;
-  }
-
-  public function countStatus($username = null)
-  {
-    $select = $this->select()->from($this->_name, array(
-        'archived' => "SUM(status = 'archived')",
-        'draft' => "SUM(status = 'draft')",
-        'pending' => "SUM(status = 'pending')",
-        'publish' => "SUM(status = 'publish')"));
-
-    if (null !== $username) {
-      $select->where("{$this->_name}.created_by = ?", $username);
-    }
-
-    $result = $this->getAdapter()->fetchRow($select);
-    return $result;
-  }
 
 }
