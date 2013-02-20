a3ef65001ecf06683be51f2b49665f5cb0f485ba
Wildan Fath Malik <fathmalik15@gmail.com>
2013-02-20 22:00:24 +0700

    yes

--- application/controllers/NewsController.php
+++ application/controllers/NewsController.php
@@ -1,24 +1,45 @@
 <?php
 
-class NewsController extends Zend_Controller_Action
-{
+class NewsController extends Zend_Controller_Action {
 
-    public function init()
-    {
+    public function init() {
         /* Initialize action controller here */
     }
 
-    public function indexAction()
-    {
+    public function indexAction() {
         // action body
-        
+
         $model = new Application_Model_DbTable_News();
         $data = $model->findAll();
-        
+
         $this->view->newss = $data;
-        
     }
 
+    public function viewAction() {
+
+        $id = $this->getParam('id');
+
+        if (null != $id) {
+            $model = new Application_Model_DbTable_News;
+
+            $news = $model->find($id)->current();
+            
+            if(null != $news){
+                
+                if($this->getRequest()->isPost()){
+                    
+                    $data = $this->getRequest()->getPost();
+                    
+                    
+                }
+                
+                $this->view->auth = Zend_Auth::getInstance();
+                $this->view->news = $news->toArray();
+                
+            }
+            
+        }
+    }
 
 }
 
--- application/models/DbTable/News.php
+++ application/models/DbTable/News.php
@@ -5,28 +5,38 @@
  * and open the template in the editor.
  */
 
-class Application_Model_DbTable_News extends Zend_Db_Table_Abstract{
+class Application_Model_DbTable_News extends Zend_Db_Table_Abstract {
+
     protected $_name = 'news';
-    
-    public function findLatestNews($limit=5){
+
+    public function findLatestNews($limit = 5) {
         $select = $this->select()->from($this->_name, array('title', 'id'));
         $select->limit($limit);
         $select->order('created_on asc');
         $select->where('status = "publish"');
         $result = $this->fetchAll($select);
-        
+
         return $result;
-        
-        
     }
-    
-    
-    public function findAll(){
+
+    public function findAll() {
         $select = $this->select()
-        ->from($this->_name);
+                ->from($this->_name);
         $result = $this->fetchall($select);
         return $result;
     }
+
+    public function findById($news_id) {
+
+        $select = $this->select()
+                ->from($this->_name)
+                ->where("{$this->_name}.id =", $news_id);
+
+        $result = $this->fetchRow($select);
+        
+        return $result;
+    }
+
 }
 
 ?>
