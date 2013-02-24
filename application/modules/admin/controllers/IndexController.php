21b5bf60f7972d49788334aa6e41d17bef4093d4
Egi Soleh Hasdi <egisolehhasdi@gmail.com>
2013-02-24 22:26:01 +0700

    Komentar terbaru di dashboard

--- application/modules/admin/controllers/IndexController.php
+++ application/modules/admin/controllers/IndexController.php
@@ -1,31 +1,33 @@
 <?php
 
-class Admin_IndexController extends Zend_Controller_Action
-{
-  /**
-   *
-   * @var Admin_Model_DbTable_Guestbook
-   */
-  protected $guestbook;
-  /**
-   *
-   * @var Admin_Model_Analytics
-   */
-//  protected $analytics;
+class Admin_IndexController extends Zend_Controller_Action {
 
-  public function init()
-  {
-    /* Initialize action controller here */
-    $this->_helper->layout->setLayout('admin');
-    $this->guestbook = new Admin_Model_DbTable_Guestbook();
-//    $this->analytics = new Admin_Model_Analytics();
-  }
+    /**
+     *
+     * @var Admin_Model_DbTable_Guestbook
+     */
+    protected $guestbook;
+    /**
+     *
+     * @var Admin_Model_DbTable_Comment
+     */
+    protected $comment;
 
-  public function indexAction()
-  {
-    // action body
-    $guestbooks = $this->guestbook->findAll(5);
-    $this->view->guestbooks = $guestbooks;
-  }
+    public function init() {
+        /* Initialize action controller here */
+        $this->_helper->layout->setLayout('admin');
+        $this->guestbook = new Admin_Model_DbTable_Guestbook();
+        $this->comment = new Admin_Model_DbTable_Comment();
+    }
+
+    public function indexAction() {
+        // action body
+        $username = Zend_Auth::getInstance()->getIdentity()->username;
+        $guestbooks = $this->guestbook->findAll(5);
+        $latestComments = $this->comment->getUserLatestComments($username);
+
+        $this->view->latestComments = $latestComments;
+        $this->view->guestbooks = $guestbooks;
+    }
 
 }
